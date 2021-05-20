acdc8a07eb83e137855350c392f41c27a2f3356f
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-05-19 21:45:46 -0300

    fifth lesson finished :tada:

---
+++ scripts/carousel.js
@@ -0,0 +1,17 @@
+var slider = tns({
+  container: '.my-slider',
+  items: 1,
+  responsive: {
+    640: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 2
+    },
+    700: {
+      gutter: 30
+    },
+    900: {
+      items: 3
+    }
+  }
+});
